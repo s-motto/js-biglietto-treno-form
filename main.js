8daea6ddffd247c9bfad7e3d3cfc097be31e8ec5
Brina <brinamotto@gmail.com>
2024-05-13 18:02:36 +0200

    [fix] if..else statement

--- main.js
+++ main.js
@@ -23,28 +23,24 @@ ticketPrice.addEventListener('submit', function (event) {
         finalPrice = Number(price - (price * overSixtyfiveDiscount)).toFixed(2);
         document.getElementById('discount').innerText = 'Complimenti, hai diritto a uno sconto del 40%!';
         document.getElementById('ticket-result').classList.toggle('d-none');
-
     }
-    else if (age !== Number || kmNumber !== Number) {
-        document.getElementById('ticket-fail').classList.toggle('d-none');
-
-    }
-
-    else {
-        finalPrice = Number(price).toFixed(2)
+    else if (age >= 18) {
+        finalPrice = Number(price).toFixed(2);
         document.getElementById('ticket-result').classList.toggle('d-none');
+    }
+    else if (!Number.isNaN(age)) {
+        document.getElementById('ticket-fail').classList.toggle('d-none');
+    }
+    else {
+        document.getElementById('ticket-fail').classList.toggle('d-none');
     };
 
-
-
-
-
     document.getElementById('final-price').innerText = `${finalPrice}`;
-
-
     console.log('ho inviato il form');
     console.log(`Il prezzo del tuo biglietto è ${finalPrice}€`);
 });
+
+
 
 document.getElementById('reset-button').addEventListener('click', function () {
     document.getElementById('ticket-result').classList.add('d-none');
